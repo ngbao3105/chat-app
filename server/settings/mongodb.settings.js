bd740853ef05574e8ddcf2dec1ef8a7905819284
Nguyen Gia Bao <baobao816840@gmail.com>
2021-09-11 01:48:10 +0700

    update env

--- server/settings/mongodb.settings.js
+++ server/settings/mongodb.settings.js
@@ -9,7 +9,7 @@ let mongodbDbName = 'chat-app';
 
 module.exports = {
     MONGODB_CLIENT: MongoDbClient,
-    MONGODB_URI: 'mongodb+srv://baonguyen:1234@demo.sfjra.mongodb.net/',
+    MONGODB_URI: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@demo.sfjra.mongodb.net/`,
     MONGODB_CLIENT_OPTIONS: { useNewUrlParser: true, useUnifiedTopology: true },
     MONGODB_DB_NAME: mongodbDbName,
     OBJECT_ID: ObjectId
