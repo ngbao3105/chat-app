7ac6fd7c3a4555f8b7c56339011d91543da794fd
Nguyen Gia Bao <baobao816840@gmail.com>
2021-09-22 01:24:21 +0700

    update service test case

--- src/app/shared/chat-service.component.spec.ts
+++ src/app/shared/chat-service.component.spec.ts
@@ -20,39 +20,67 @@ fdescribe('ChatServiceService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
-
-    it('postBody should send a POST reqeust and return the posted item', (done) => {
-        service.postBody({
-            channelId: '1',
-            text: 'mock unit test http request from angular app',
-            user: {
-                _id: "Russell",
-                userName: "Russell",
-                avatar: "Russell.png"
-            }
-        }).subscribe((item) => {
-            expect(item).toBeDefined;
-            done();
+    fdescribe('postBody', () => {
+        it('postBody should send a POST reqeust and return the posted item', (done) => {
+            service.postBody({
+                channelId: '1',
+                text: 'mock unit test http request from angular app',
+                user: {
+                    _id: "Russell",
+                    userName: "Russell",
+                    avatar: "Russell.png"
+                }
+            }).subscribe((item) => {
+                expect(item).toBeDefined();
+                done();
+            }, (error) => {
+                fail("The request was supposed to return an empty array");
+                done();
+            });
+            const testRequest = httpMock.expectOne({ url: "http://localhost:3000/graphql", method: "POST" });
+            expect(testRequest.request.method).toBe("POST");
+            expect(testRequest.request.body).toEqual({
+                channelId: '1',
+                text: 'mock unit test http request from angular app',
+                user: {
+                    _id: "Russell",
+                    userName: "Russell",
+                    avatar: "Russell.png"
+                }
+            });
+            testRequest.flush({
+                channelId: '1',
+                text: 'mock unit test http request from angular app',
+                user: {
+                    _id: "Russell",
+                    userName: "Russell",
+                    avatar: "Russell.png"
+                }
+            })
         });
-        const testRequest = httpMock.expectOne("http://localhost:3000/graphql");
-        expect(testRequest.request.method).toBe("POST");
-        expect(testRequest.request.body).toEqual({
-            channelId: '1',
-            text: 'mock unit test http request from angular app',
-            user: {
-                _id: "Russell",
-                userName: "Russell",
-                avatar: "Russell.png"
-            }
-        });
-        testRequest.flush({
-            channelId: '1',
-            text: 'mock unit test http request from angular app',
-            user: {
-                _id: "Russell",
-                userName: "Russell",
-                avatar: "Russell.png"
-            }
+        it("should return an empty array if an Internal Server Error occurs", (done) => {
+            service.postBody({
+                channelId: '1',
+                text: 'mock unit test http request from angular app',
+                user: {
+                    _id: "Russell",
+                    userName: "Russell",
+                    avatar: "Russell.png"
+                }
+            }).subscribe((item) => {
+                expect(item).toBeDefined();
+                expect(Array.isArray(item)).toBe(true);
+                expect(item.length).toBe(0);
+                done();
+            }, (error) => {
+                fail("The request was supposed to return an empty array");
+                done();
+            });
+            const testRequest = httpMock.expectOne({ url: "http://localhost:3000/graphql", method: "POST" });
+            expect(testRequest.request.method).toBe("POST");
+            testRequest.flush({}, { status: 500, statusText: "Internal Server Error" })
         })
     })
+
+
 });
--- src/app/shared/chat-service.service.ts
+++ src/app/shared/chat-service.service.ts
@@ -1,16 +1,22 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
 
-  constructor(@Inject(HttpClient) private http: HttpClient)  { }
+  constructor(@Inject(HttpClient) private http: HttpClient) { }
   private graphqlUrl = "http://localhost:3000/graphql";
-  postBody(body) : Observable<any>{
-    const headers = { 'Content-Type': 'application/json'  };
-    return this.http.post<any>(this.graphqlUrl, body, { headers });
+  postBody(body): Observable<any> {
+    const headers = { 'Content-Type': 'application/json' };
+    return this.http.post<any>(this.graphqlUrl, body, { headers }).pipe(
+      catchError((error) => {
+        if (error.status === 500)
+          return of([]);
+      })
+    );
   }
 }
